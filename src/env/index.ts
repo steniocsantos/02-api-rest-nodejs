eb89ff0e7b1274793c08576375e4475bdc9aa519
steniocsantos <steniocsantos@gmail.com>
2024-08-05 23:22:12 -0300

    chore: Production commit

--- src/env/index.ts
+++ src/env/index.ts
@@ -8,8 +8,9 @@ if(process.env.NODE_ENV === 'test'){
 
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+    DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
     DATABASE_URL: z.string(),
-    PORT: z.number()
+    PORT: z.coerce.number()
         .default(3333)
 })
 
