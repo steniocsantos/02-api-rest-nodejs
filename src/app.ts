3e32f4c90af1b8596bd4af08f5a35319edf2b191
steniocsantos <steniocsantos@gmail.com>
2024-08-05 22:34:15 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import fastify from 'fastify';
+import { env } from './env';
+import { transactionRoutes } from './routes/transactions';
+import cookie from '@fastify/cookie'
+
+export const app = fastify()
+
+app.register(cookie)
+
+app.register(transactionRoutes, {
+    prefix: 'transactions',
+})
