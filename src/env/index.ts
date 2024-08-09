faee8760e8d151f532fc9ec90b0015b24ec6c2b7
steniocsantos <steniocsantos@gmail.com>
2024-08-09 07:15:48 -0300

    fix: alteracao package.json

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,8 @@
 import { config } from 'dotenv'
 import { z } from 'zod'
 
-if(process.env.NODE_ENV === 'test'){
-    config({path: '.env.test'})
+if (process.env.NODE_ENV === 'test') {
+    config({ path: '.env.test' })
 } else config()
 
 
@@ -11,7 +11,7 @@ const envSchema = z.object({
     DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
     DATABASE_URL: z.string(),
     PORT: z.coerce.number()
-        .default(3333)
+        .default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
